b5c1aab4c1e5249831a8fd571a7099689bbe6cc3
Smriti-sh <smritisapr2001@gmail.com>
2024-06-08 16:26:22 +0530

    added cursor focus and enter key function

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const inputBox = document.getElementById("input-box");
 const taskList = document.getElementById("task-list");
 const addBtn = document.getElementById("add-Task");
 
-addBtn.addEventListener("click", function (event) {
+inputBox.focus(); //one time thing
+inputBox.select(); // always
+
+inputBox.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.key === 13) {
+    click();
+  }
+});
+
+addBtn.addEventListener("click", function click(event) {
+  event.preventDefault();
   if (inputBox.value.trim() === "") {
     alert("You must first enter something");
   } else {
@@ -13,7 +24,6 @@ addBtn.addEventListener("click", function (event) {
     span.innerHTML = "&times;";
     li.appendChild(span);
     // console.log(event, "event");
-    event.preventDefault();
   }
   inputBox.value = "";
   saveData();
@@ -40,3 +50,46 @@ function showTask() {
   taskList.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+// const inputBox = document.getElementById("input-box");
+// const taskList = document.getElementById("task-list");
+// const addBtn = document.getElementById("add-Task");
+
+// addBtn.addEventListener("click", function (event) {
+//   if (inputBox.value.trim() === "") {
+//     alert("You must first enter something");
+//   } else {
+//     var li = document.createElement("li");
+//     li.innerHTML = inputBox.value;
+//     taskList.appendChild(li);
+//     var span = document.createElement("span");
+//     span.innerHTML = "&times;";
+//     li.appendChild(span);
+//     // console.log(event, "event");
+//     event.preventDefault();
+//   }
+//   inputBox.value = "";
+//   saveData();
+// });
+
+// taskList.addEventListener(
+//   "click",
+//   function (e) {
+//     if (e.target.tagName === "LI") {
+//       e.target.classList.toggle("checked");
+//       saveData();
+//     } else if (e.target.tagName === "SPAN") {
+//       e.target.parentElement.remove();
+//       saveData();
+//     }
+//   },
+//   false
+// );
+
+// function saveData() {
+//   localStorage.setItem("data", taskList.innerHTML);
+// }
+// function showTask() {
+//   taskList.innerHTML = localStorage.getItem("data");
+// }
+// showTask();
